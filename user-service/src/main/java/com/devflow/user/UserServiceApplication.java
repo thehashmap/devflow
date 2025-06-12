6c42f141133da180be5ae0cad291edc90ded55df
Nikhil Kumar <nkmnikhil0009@gmail.com>
2025-06-12 08:35:34 +0530

    user-service security config

--- user-service/src/main/java/com/devflow/user/UserServiceApplication.java
+++ user-service/src/main/java/com/devflow/user/UserServiceApplication.java
@@ -2,8 +2,13 @@ package com.devflow.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.devflow.user.repository")
+@ComponentScan(basePackages = "com.devflow.user")
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
